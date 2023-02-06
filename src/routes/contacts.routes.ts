bb2176c87fa591e6bac54ea41cf53883a84a3dc2
byetevinn <stevan14padilha@gmail.com>
2023-02-06 19:37:14 -0300

    Feat: Delete contact

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -5,6 +5,7 @@ import ensureAuth from "../middlewares/ensureAuth.middleware";
 import listContactsController from "../controllers/contacts/listContacts.controller";
 import updateContactController from "../controllers/contacts/updateContact.controller";
 import createContactController from "../controllers/contacts/createContact.controller";
+import deleteContactController from "../controllers/contacts/deleteContact.controller";
 
 const routes = Router();
 
@@ -12,6 +13,7 @@ const contactsRoutes = () => {
   routes.post("", ensureAuth, createContactController);
   routes.get("", ensureAuth, listContactsController);
   routes.patch("", ensureAuth, updateContactController);
+  routes.delete("", ensureAuth, deleteContactController);
 
   return routes;
 };
