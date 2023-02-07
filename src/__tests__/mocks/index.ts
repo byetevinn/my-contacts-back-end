42c2ba15f725f8df76f0d67ba532461ef39cbc8e
byetevinn <stevan14padilha@gmail.com>
2023-02-07 20:18:37 -0300

    Feat: All tests clients

--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,8 +1,20 @@
-import { IClientRequest } from "../../interfaces/clients";
+import { IClientLogin, IClientRequest } from "../../interfaces/clients";
 
 export const mockedClient: IClientRequest = {
   email: "andrade@gmail.com",
   password: "Andrade12!@",
   fullName: "Andrade Padilha",
   phone: "41998765432",
+};
+
+export const mockedClientLogin: IClientLogin = {
+  email: "andrade@gmail.com",
+  password: "Andrade12!@",
+};
+
+export const mockedOtherClient: IClientRequest = {
+  email: "eric@gmail.com",
+  password: "Eric12!@",
+  fullName: "Eric Padilha",
+  phone: "41998865423",
 };
