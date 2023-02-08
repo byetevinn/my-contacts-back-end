f7b198a72aab09b9b1050e60b106e382c2532d5c
byetevinn <stevan14padilha@gmail.com>
2023-02-07 22:33:17 -0300

    Feat: Id required to delete contact

--- src/services/contacts/deleteContact.service.ts
+++ src/services/contacts/deleteContact.service.ts
@@ -4,6 +4,10 @@ import { contactRepository } from "../../utilities/repositories";
 const deleteContactService = async (id: string) => {
   const contact = await contactRepository.findOneBy({ id });
 
+  if (!id) {
+    throw new AppError("Id required");
+  }
+
   if (!contact) {
     throw new AppError("The contact has already been deleted");
   }
