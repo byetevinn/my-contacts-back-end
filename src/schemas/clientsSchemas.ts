fe289c1fd78283377cb88c733448a39f8e3f72e7
byetevinn <stevan14padilha@gmail.com>
2023-02-11 22:48:41 -0300

    Fix: Validations schemas

--- src/schemas/clientsSchemas.ts
+++ src/schemas/clientsSchemas.ts
@@ -31,5 +31,10 @@ export const updateClientSchema = yup.object().shape({
   phone: yup
     .string()
     .notRequired()
-    .matches(/^[0-9]+$/, "Only numbers"),
+    .matches(/^[0-9]*$/, "Only numbers")
+    .test("phone", "Must contain at least 9 digits", (value) => {
+      if (!value) return true;
+      return value.length >= 9;
+    })
+    .max(11, "Must contain a maximum of 11 digits"),
 });
